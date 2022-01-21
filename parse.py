c2bd4915ecfec2930f80e90adf240a8ce8e646cc
Adrian <adriancristea89@yahoo.com>
2022-01-21 17:57:15 +0000

    Adds get_minutes method

--- parse.py
+++ parse.py
@@ -1,6 +1,6 @@
 import sys
 
-def parse_cron():
+def parse_cron() -> None:
     
     try:    
         arguments = sys.argv[1].split()
@@ -8,12 +8,25 @@ def parse_cron():
         if len(arguments) != 6:
             print(f"Invalid command. There should be 6 arguments, following the pattern: {command_format}")
             return
+        
+        result = {
+            'minutes' : get_minutes(arguments[0])
+        }
+
     except IndexError:
         print(f"Invalid command. The command should be ran with a string of arguments.")
         return
-    except Exception:
-        print('Something went wrong!')
+    except Exception as e:
+        print(f"Something went wrong: {e}")
         return
+
+def get_minutes(minute_string: str) -> str:
+    
+    if '*/' in minute_string:
+        minute_string = minute_string.replace('*/', '')
+        minutes_list = [str(i) for i in range(0, 60, int(minute_string))]
+        return ' '.join(minutes_list)
+    return minute_string
 
 if __name__ == '__main__':
     parse_cron()
